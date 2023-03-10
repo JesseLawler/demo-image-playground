1218f3c0c102f705d2d4e62951bd67ea6375da4d
EvilGeniusTechnologies <Jesse@EvilGeniusTechnologies.com>
2023-03-10 13:44:49 -0800

    basic drag-and-drop working great

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {
   Animated,
+  Dimensions,
   Image,
   ImageBackground,
   PanResponder,
@@ -11,10 +12,12 @@ import {
 } from 'react-native';
 import RNPhotoManipulator from 'react-native-photo-manipulator';
 
-const DROP_AREA_HEIGHT = 250;
-const DROP_AREA_WIDTH = 360;
-
+const CIRCLE_RADIUS = 40;
+const DEVICE_WIDTH = Dimensions.get('window').width;
 const BG_IMAGE_SOURCE = require('./src/assets/images/profile-bg-bw.jpg');
+const DROP_AREA_HEIGHT = 250;
+const MINIMUM_SIZE_RATIO = 0.1;
+const OPACITY_AFTER_DROP = 0.2;
 
 const imageSource: any = (name: string) => {
   switch (name) {
@@ -26,6 +29,10 @@ const imageSource: any = (name: string) => {
       return require('./src/assets/images/fa/bomb.png');
     case 'bullseye':
       return require('./src/assets/images/fa/bullseye.png');
+    case 'caret-right':
+      return require('./src/assets/images/fa/caret-right.png');
+    case 'green-square':
+      return require('./src/assets/images/green-square.png');
   }
 };
 
@@ -36,6 +43,7 @@ type xyCoordinates = {
 
 interface DraggableProps {
   imageName?: string;
+  displaySizeProportion: number;
   dropBehavior?: Function | null;
 }
 
@@ -43,30 +51,35 @@ interface DraggableState {
   showDraggable: boolean;
   pan: any;
   opacity: any;
+  height: number;
+  width: number;
 }
 
 class Draggable extends Component<DraggableProps, DraggableState> {
   panResponder: any;
 
+  private _ref: any; // JESSEFIX
   private _val: xyCoordinates;
+  private _startLocation: xyCoordinates;
 
   constructor(props: any) {
     super(props);
 
     this._val = {x: 0, y: 0};
+    this._startLocation = {x: 0, y: 0};
 
     let pan = new Animated.ValueXY();
     pan.addListener((value: xyCoordinates) => {
       this._val = value;
     });
 
-    console.log('imageName: ' + this.props.imageName);
-
     this.state = {
       showDraggable: true,
       //dropAreaValues: null,
       pan: pan,
       opacity: new Animated.Value(1),
+      height: 0,
+      width: 0,
     };
 
     this.panResponder = PanResponder.create({
@@ -84,30 +97,96 @@ class Draggable extends Component<DraggableProps, DraggableState> {
       ),
       onPanResponderRelease: (e, gesture) => {
         if (this.isDropArea(gesture)) {
-          console.log('Dropped at: ' + this._val.x + ', ' + this._val.y);
-          if (this.props.dropBehavior)
-            this.props.dropBehavior(this.props.imageName, this._val);
+          if (this.props.dropBehavior) {
+            // Determine the relative position of the dropped Draggable vs.
+            // the page origin (which happens to be the drop-image's origin, too).
+            let adjustedCoords = {
+              x:
+                (this._val.x + this._startLocation.x) /
+                this.props.displaySizeProportion,
+              y:
+                (this._val.y + this._startLocation.y) /
+                this.props.displaySizeProportion,
+            };
+            console.log(`Dropped at: ${adjustedCoords.x}, ${adjustedCoords.y}`);
+            this.props.dropBehavior(this.props.imageName, adjustedCoords);
+          }
           Animated.timing(this.state.opacity, {
-            toValue: 0,
+            toValue: OPACITY_AFTER_DROP,
             duration: 1000,
             useNativeDriver: true,
-          }).start(() =>
+          }).start(() => {
             this.setState({
-              showDraggable: false,
-            }),
-          );
+              showDraggable: OPACITY_AFTER_DROP > 0,
+            });
+          });
         }
       },
     });
   }
 
+  componentDidMount(): void {
+    const temp = Image.resolveAssetSource(imageSource(this.props.imageName));
+    const ratio = Math.max(
+      MINIMUM_SIZE_RATIO,
+      this.props.displaySizeProportion,
+    );
+    console.log(
+      'this.props.displaySizeProportion: ' + this.props.displaySizeProportion,
+    );
+    this.setState(
+      {
+        height: Math.round(temp.height * ratio),
+        width: Math.round(temp.width * ratio),
+      },
+      () =>
+        console.log(
+          'Image ' +
+            this.props.imageName +
+            ' resized to ' +
+            this.state.width +
+            ' x ' +
+            this.state.height,
+        ),
+    );
+  }
+
   isDropArea(gesture: PanResponderGestureState) {
-    return gesture.moveY < DROP_AREA_HEIGHT;
+    return gesture.moveY < DROP_AREA_HEIGHT; // JESSEFIX
   }
 
   render() {
     return (
-      <View style={{width: '20%', alignItems: 'center'}}>
+      <View
+        ref={ref => (this._ref = ref)}
+        style={{
+          width: this.state.width,
+          height: this.state.height,
+        }}
+        onLayout={({nativeEvent}) => {
+          if (this._ref) {
+            this._ref.measure(
+              (
+                x: number,
+                y: number,
+                width: number,
+                height: number,
+                pageX: number,
+                pageY: number,
+              ) => {
+                this._startLocation = {x: pageX, y: pageY};
+                console.log(
+                  'icon ' +
+                    this.props.imageName +
+                    "'s start location is: " +
+                    pageX +
+                    ', ' +
+                    pageY,
+                );
+              },
+            );
+          }
+        }}>
         {this.renderDraggable()}
       </View>
     );
@@ -121,7 +200,14 @@ class Draggable extends Component<DraggableProps, DraggableState> {
       <Animated.Image
         {...this.panResponder.panHandlers}
         source={imageSource(this.props.imageName)}
-        style={[panStyle, styles.draggableImage, {opacity: this.state.opacity}]}
+        style={[
+          panStyle,
+          {
+            width: this.state.width,
+            height: this.state.height,
+            opacity: this.state.opacity,
+          },
+        ]}
       />
     ) : (
       <Animated.View
@@ -140,8 +226,11 @@ interface AppProps {
 }
 
 interface AppState {
-  imageWidth: number | null;
+  arrayOfIconNames?: string[];
+  displayProportion: number;
+  dropAreaHeight: number;
   mergedImagePath: string;
+  naturalImageDimensions: xyCoordinates | null;
 }
 
 export default class App extends Component<AppProps, AppState> {
@@ -149,9 +238,24 @@ export default class App extends Component<AppProps, AppState> {
     super(props);
 
     this.state = {
-      imageWidth: null,
+      displayProportion: 0,
+      dropAreaHeight: 0,
       mergedImagePath: '',
+      naturalImageDimensions: null,
     };
+  }
+
+  componentDidMount(): void {
+    const temp = Image.resolveAssetSource(BG_IMAGE_SOURCE);
+    const relativeHeight = Math.round(
+      (temp.height / temp.width) * DEVICE_WIDTH,
+    );
+    this.setState({
+      arrayOfIconNames: ['green-square', 'arrow-up', 'caret-right'],
+      displayProportion: Math.round((DEVICE_WIDTH * 1000) / temp.width) / 1000,
+      dropAreaHeight: relativeHeight,
+      naturalImageDimensions: {x: temp.width, y: temp.height},
+    });
   }
 
   mergeImage = (name: string, coords: xyCoordinates) => {
@@ -167,38 +271,64 @@ export default class App extends Component<AppProps, AppState> {
   };
 
   render() {
+    let details =
+      'natural dimensions: ' +
+      this.state.naturalImageDimensions?.x +
+      ' x ' +
+      this.state.naturalImageDimensions?.y +
+      '\n' +
+      'display dimensions: ' +
+      DEVICE_WIDTH +
+      ' x ' +
+      this.state.dropAreaHeight +
+      '\n' +
+      'display percent: ' +
+      Math.round(1000 * this.state.displayProportion) / 10 +
+      '%';
+
+    const mergedImage = this.state.mergedImagePath ? (
+      <Image
+        source={{uri: this.state.mergedImagePath}}
+        resizeMode={'cover'}
+        style={{width: DEVICE_WIDTH, height: this.state.dropAreaHeight}}
+      />
+    ) : null;
+
     return (
       <View style={styles.mainContainer}>
         <View style={styles.dropZone}>
           <ImageBackground
             source={BG_IMAGE_SOURCE}
-            style={{height: DROP_AREA_HEIGHT}}
-            onLayout={event => {
-              var {x, y, width, height} = event.nativeEvent.layout;
-              this.setState({imageWidth: width});
-            }}>
+            style={{height: this.state.dropAreaHeight}}
+            resizeMode="contain">
             <Text style={styles.text}>Drop them here!</Text>
-            <Text style={styles.dropAreaDetails}>
-              image dimensions: {this.state.imageWidth} x {DROP_AREA_HEIGHT}
+            <Text style={styles.dropAreaDetails} numberOfLines={3}>
+              {details}
             </Text>
           </ImageBackground>
         </View>
         <View style={styles.row}>
-          <Draggable imageName={'bullseye'} dropBehavior={this.mergeImage} />
-          <Draggable imageName={'bomb'} dropBehavior={this.mergeImage} />
-          <Draggable imageName={'arrow-up'} dropBehavior={this.mergeImage} />
+          {this.state.arrayOfIconNames?.map((iconName: string) => (
+            <Draggable
+              key={iconName}
+              imageName={iconName}
+              dropBehavior={this.mergeImage}
+              displaySizeProportion={this.state.displayProportion}
+            />
+          ))}
         </View>
-        <Image
-          source={{uri: this.state.mergedImagePath}}
-          resizeMode={'cover'}
-          style={styles.mergedImage}
-        />
+        <View
+          style={{
+            width: DEVICE_WIDTH,
+            height: this.state.dropAreaHeight,
+            marginBottom: 30,
+          }}>
+          {mergedImage}
+        </View>
       </View>
     );
   }
 }
-
-let CIRCLE_RADIUS = 40;
 
 const styles = StyleSheet.create({
   mainContainer: {
@@ -213,31 +343,23 @@ const styles = StyleSheet.create({
     height: CIRCLE_RADIUS * 2,
     borderRadius: CIRCLE_RADIUS,
   },
-  draggableImage: {
-    width: CIRCLE_RADIUS * 2,
-    height: CIRCLE_RADIUS * 2,
-  },
   row: {
+    width: DEVICE_WIDTH,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
   },
   dropZone: {
-    height: DROP_AREA_HEIGHT,
-    width: DROP_AREA_WIDTH,
+    width: DEVICE_WIDTH,
     backgroundColor: '#00334d',
   },
   dropAreaDetails: {
     color: '#777777',
     fontSize: 15,
+    lineHeight: 21,
     width: '100%',
     textAlign: 'center',
     position: 'absolute',
-    bottom: -25,
-  },
-  mergedImage: {
-    height: DROP_AREA_HEIGHT,
-    width: DROP_AREA_WIDTH,
-    marginBottom: 30,
+    bottom: -70,
   },
   text: {
     marginTop: 60,
